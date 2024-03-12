5f23b9d52e3254645d1a2d925839deb8e92c64c9
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-11 22:14:58 -0400

    :ballot_box_with_check: last-stone-weight [Runtime: 1 ms; Memory: 41.6 MB]

---
+++ array/last-stone-weight.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int stone : stones){
+            maxHeap.add(stone);
+        }
+
+        int x = 0;
+        int y = 0;
+        while(maxHeap.size() > 1){
+            y = maxHeap.poll();
+            x = maxHeap.poll();
+            if(y > x){
+                maxHeap.offer(y - x);
+            }
+        }
+
+        //If the maxHeap is empty, the expression evaluates to 0
+        //If the maxHeap is not empty, the expression evaluates to maxHeap.poll()
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
