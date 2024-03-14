7732591ddf97b3c11ffd8f73feea1889d1b65951
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-14 03:49:06 -0400

    :ballot_box_with_check: remove-element [Runtime: 0 ms; Memory: 41.9 MB]

--- two-pointers/remove-element.java
+++ two-pointers/remove-element.java
@@ -1,5 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        //test case: [3,2,2,3] index = 2, i = 3
+        //test case: [2,2,2,3]
+
+        //test case: [3,3] index = 0, i = 2
+        //test case: [3,3]
+        
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
