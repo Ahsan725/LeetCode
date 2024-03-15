5ac0ae64363e621df705b67a04a5f567afdf36a8
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-15 02:13:33 -0400

    :ballot_box_with_check: majority-element [Runtime: 2 ms; Memory: 53 MB]

--- array/majority-element.java
+++ array/majority-element.java
@@ -1,7 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
+        //I will use voting algo instead for this to get O(n)
 
-        return nums[nums.length/2];
+        int count = 0;
+        int candidate = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+
+            if(candidate == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidate; 
     }
 }
