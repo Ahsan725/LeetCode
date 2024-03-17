e23025ea7752182f7c653c8ee5f0650aa6aade8a
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-17 04:04:10 -0400

    :ballot_box_with_check: reverse-words-in-a-string [Runtime: 27 ms; Memory: 16.9 MB]

---
+++ two-pointers/reverse-words-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()#splits the words on space so we should have: the sky is blue
+
+        result = ""
+
+        for i in range(len(words)-1, -1, -1):
+            if i == len(words)-1:
+                result += "" + words[i]
+            else:
+                result += " " + words[i]
+
+        return result
+        
