d25a655d0dd325bdc939e4cd6b2c252f6fbbdc9b
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-17 04:51:19 -0400

    :ballot_box_with_check: find-the-index-of-the-first-occurrence-in-a-string [Runtime: 30 ms; Memory: 16.5 MB]

--- two-pointers/find-the-index-of-the-first-occurrence-in-a-string.py
+++ two-pointers/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -2,6 +2,9 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
         for i in range(len(haystack)):
-            if haystack[i:i + len(needle)] == needle:
+            if haystack[i:i + len(needle)] == needle: 
+        #splice a substring starting at index i to the length of needle so
+        #if needle was 3 letters It will splice a 3 letter substring starting
+        #at index i, which will them be matched with the word we are looking for
                 return i
         return -1
