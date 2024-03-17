8483e4cf5917b4c1f5dba61d3724689e768a48c0
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-17 03:34:13 -0400

    :ballot_box_with_check: length-of-last-word [Runtime: 31 ms; Memory: 16.6 MB]

--- string/length-of-last-word.py
+++ string/length-of-last-word.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        words = s.split()
-        return len(words[-1])
+        words = s.split() #splits on space by default
+        return len(words[-1]) #words[-1] this gives you last elements in the array words[-2]->2nd last
 
         
