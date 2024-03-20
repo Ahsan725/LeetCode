3dd36d9b2755fbe6c1dee8bf42de906b4273e25d
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-19 22:05:13 -0400

    :ballot_box_with_check: best-time-to-buy-and-sell-stock [Runtime: 726 ms; Memory: 27.3 MB]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -5,7 +5,8 @@ class Solution:
         max_profit = 0
     
         for price in prices:
-            min_price = min(min_price, price)
+            if min_price > price:
+                min_price = price
             max_profit = max(max_profit, price - min_price)
         
         return max_profit
