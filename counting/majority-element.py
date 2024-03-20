f8239bc2958dc44a64fdb7b900857f64c679bf6b
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-19 21:49:19 -0400

    :ballot_box_with_check: majority-element [Runtime: 159 ms; Memory: 18.1 MB]

--- counting/majority-element.py
+++ counting/majority-element.py
@@ -10,7 +10,8 @@ class Solution:
         for i in range(len(nums)):
             if count == 0:
                 candidate = nums[i]
-            if nums[i] == candidate:
+                count +=1
+            elif nums[i] == candidate:
                 count += 1
             else:
                 count -= 1
