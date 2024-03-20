e9c122939604ff82262e0479a4f834df9fba0f8a
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-19 22:08:53 -0400

    :ballot_box_with_check: best-time-to-buy-and-sell-stock [Runtime: 669 ms; Memory: 27.2 MB]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-    
-        min_price = prices[0]
+        # Initialize min_price to the price of the first day
+        min_price = prices[0] 
+        
+        # Initialize max_profit to 0
         max_profit = 0
     
+        # Iterate through each price in the prices array
         for price in prices:
+            # Update min_price if the current price is lower than min_price
             if min_price > price:
                 min_price = price
+            
+            # Update max_profit if the profit obtained by selling at the current price
+            # (current price - min_price) is greater than the current max_profit
             if max_profit < price - min_price:
                 max_profit = price - min_price
         
+        # Return the maximum profit obtained
         return max_profit
