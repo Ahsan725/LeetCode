b1bbf8c67ea475ba47e1d50509dc22e125ff3dee
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-19 21:47:32 -0400

    :ballot_box_with_check: majority-element [Runtime: 180 ms; Memory: 18 MB]

--- counting/majority-element.py
+++ counting/majority-element.py
@@ -1,5 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # sorted_nums = sorted(nums) -> this one returns a new sorted list 
-        nums.sort() # -> this one sorts the same list 
-        return nums[(len(nums)//2)]
+        # # sorted_nums = sorted(nums) -> this one returns a new sorted list 
+        # nums.sort() # -> this one sorts the same list 
+        # return nums[(len(nums)//2)] 
+
+
+        candidate = 0
+        count = 0
+        for i in range(len(nums)):
+            if count == 0:
+                candidate = nums[i]
+            if nums[i] == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
