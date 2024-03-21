bf1af0cad03182a90a7821097bbf96f6edddd30f
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-21 04:25:34 -0400

    :ballot_box_with_check: group-anagrams [Runtime: 93 ms; Memory: 22.3 MB]

--- sorting/group-anagrams.py
+++ sorting/group-anagrams.py
@@ -2,7 +2,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
 
-        for word in strs:
-            sorted_word = ''.join(sorted(word))
-            anagrams[sorted_word].append(word)
-        return list(anagrams.values())
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] +=1
+            key = tuple(count)
+            anagrams[key].append(s)
+        return anagrams.values()
