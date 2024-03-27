5e838adf0df18e8b37ff3b2eb5e23f19c94e9c6b
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-03-27 03:59:49 -0400

    :ballot_box_with_check: valid-palindrome [Runtime: 47 ms; Memory: 17.2 MB]

---
+++ string/valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # string = (str.strip(s)).lower()
+        string = ''.join(e for e in s if e.isalnum())
+        string = string.lower()
+
+        left = 0
+        right = len(string) - 1
+        while(left <= right):
+            if string[left] != string[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+        
