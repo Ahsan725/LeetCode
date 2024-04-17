0eaca8ce96dcc12277fdee720428a13ca3ecfd62
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-04-17 16:17:06 -0400

    :ballot_box_with_check: ransom-note [Runtime: 66 ms; Memory: 16.7 MB]

--- hash-table/ransom-note.py
+++ hash-table/ransom-note.py
@@ -2,9 +2,18 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
         # Create Counter objects for the ransomNote and magazine strings
-        note, mag = Counter(ransomNote), Counter(magazine)
+        # note, mag = Counter(ransomNote), Counter(magazine)
+        note = {}
+        mag = {}
+
+        for c in ransomNote:
+            note[c] = note.get(c,0) +1
+        for c in magazine:
+            mag[c] = mag.get(c,0) +1
         
         # Check if the intersection of note and mag Counter objects is equal to note Counter object
         # If it is, it means that all the letters in ransomNote can be formed using the letters in magazine
-        if note & mag == note: return True
-        return False
+        for key, value in note.items():
+            if key not in mag or mag[key] < value:
+                return False
+        return True
