b35b12a4db62361542bab477efd33df316064be3
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-06-05 15:57:11 -0400

    :ballot_box_with_check: valid-anagram [Runtime: 47 ms; Memory: 16.9 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,7 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_freq = Counter(s)
-        t_freq = Counter(t)
+        #practice look at the solution before this one 
 
-        return s_freq == t_freq
+        smap = {}
+        tmap = {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            smap[s[i]] = smap.get(s[i], 0) + 1
+            tmap[t[i]] = tmap.get(t[i], 0) + 1
+        
+        if smap == tmap:
+            return True
+        else:
+            return False
         
