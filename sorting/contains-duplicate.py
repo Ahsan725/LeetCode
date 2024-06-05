c7be152b09c335f1742111d3587d30d47e126492
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-06-05 15:51:23 -0400

    :ballot_box_with_check: contains-duplicate [Runtime: 417 ms; Memory: 31.8 MB]

--- sorting/contains-duplicate.py
+++ sorting/contains-duplicate.py
@@ -1,9 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        #this is a retry for practice use the solution before this one 
+
         distinct = set()
 
         for num in nums:
             distinct.add(num)
-        if len(distinct) == len(nums):
-            return False
-        else: return True
+        
+        return len(distinct) != len(nums)
+
+        
