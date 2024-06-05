994909f0441271852cfcb3f4e7b0a66034811301
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-06-05 15:26:04 -0400

    :ballot_box_with_check: longest-repeating-character-replacement [Runtime: 82 ms; Memory: 16.7 MB]

--- sliding-window/longest-repeating-character-replacement.py
+++ sliding-window/longest-repeating-character-replacement.py
@@ -12,11 +12,17 @@ class Solution:
         for r in range(len(s)):
             #s[r] will return the character at index 0 in the string s first then ++ so A
             newmap[s[r]] = newmap.get(s[r], 0) + 1
-            #generate a frequenecy map
+            #b/w the prev maxfreq and the newly incrmntd value in the map, bigger is used
             maxfreq = max(maxfreq, newmap[s[r]])
 
+            #this represents the number of replacements we have to make. If greater than k
+            #we have to increase the left pointer
             while (r - l + 1) - maxfreq > k:
+                #decrement because we are removing the first character of string from
+                #consideration. Map now displays the freq of new string under consideration
                 newmap[s[l]] -= 1
+                #advance the l pointer to remove 
+                #Now string s that will be considered will start at index 1
                 l += 1
             result = max(result, r-l + 1)
         return result
