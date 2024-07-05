88bd09ab9535bcc594c3647454a6d71438bddf7c
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-07-05 11:06:05 -0400

    :ballot_box_with_check: two-sum [Runtime: 62 ms; Memory: 17.7 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -2,13 +2,16 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #approach: Cretaing a hash map. iterate over it. For each element find its complement
         #if complmenet is found return tnose two indices
+        #key -> the number
+        #value -> the index
+        #what we are returning -> index
 
-        mymap = {}
-        
+        nummap = {}
+
         for i in range(len(nums)):
             comp = target - nums[i]
-            if comp in mymap:
-                return [mymap[comp],i]
+            if comp in nummap:
+                return [nummap[comp], i]
             else:
-                mymap[nums[i]] = i
+                nummap[nums[i]] = i
             
