342a6e975336e046036355be5145d16281ad2e03
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-09-03 09:54:25 -0400

    LC-valid-parentheses [Runtime: 39 ms; Memory: 16.4 MB]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -1,15 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = [] #initialize the stack 
-        for char in s: #go through character in the string s 
-            if char in '({[': # check if they belong to the opening brackets
-                stack.append(char) #if they do add them to the stack at the end 
-            else: # if they are closing brackets
-                if not stack: #if we have never encontered an opening bracket which would mean it could never be valid
-                    return False #so we return false immediately
-                top = stack.pop() #if we have seen an opening bracket, pop it which basically means examine it 
-                if (char == ')' and top != '(') or \
-                   (char == '}' and top != '{') or \
-                   (char == ']' and top != '['): #if the character is opening & it NOT matches with its kind of closing
-                    return False #return false
-        return not stack #if everything has been matched up it will empty the stack and not return false at any point which means we can just return true if stack is empty or false if it is not. 
+        stack = []
+
+        for char in s:
+            if char in "([{":
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+
+                if char == ')' and top != '(' or char == ']' and top != '[' or char == '}' and top != '{':
+                    return False      
+        return not stack
+
+
