baaf02bfc308d94e245a60c56c96953a49cd2195
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-09-08 20:54:49 -0400

    LC-valid-sudoku [Runtime: 104 ms; Memory: 16.4 MB]

--- matrix/valid-sudoku.py
+++ matrix/valid-sudoku.py
@@ -1,12 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def has_duplicates( block: List[str]) -> bool:
+        # Check if there are any duplicate numbers in the block
+            return len(block) != len(set(block))
         # Check all rows
         for i in range(9):
             row = []
             for j in range(9):
                 if board[i][j] != '.':
                     row.append(board[i][j])
-            if self.has_duplicates(row):
+            if has_duplicates(row):
                 return False
 
         # Check all columns
@@ -15,7 +18,7 @@ class Solution:
             for j in range(9):
                 if board[j][i] != '.':
                     column.append(board[j][i])
-            if self.has_duplicates(column):
+            if has_duplicates(column):
                 return False
 
         # Check all 3x3 sub-grids
@@ -26,12 +29,10 @@ class Solution:
                     for y in range(j, j + 3):
                         if board[x][y] != '.':
                             sub_grid.append(board[x][y])
-                if self.has_duplicates(sub_grid):
+                if has_duplicates(sub_grid):
                     return False
 
         # If no duplicates found, the Sudoku is valid
         return True
 
-    def has_duplicates(self, block: List[str]) -> bool:
-        # Check if there are any duplicate numbers in the block
-        return len(block) != len(set(block))
+
