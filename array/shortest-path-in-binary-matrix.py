a29c5df8cbe1f7c8e4e4cd28a6afd4ab81f5765a
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-10-28 17:10:01 -0400

    LC-shortest-path-in-binary-matrix [Runtime: N/A; Memory: N/A]

--- array/shortest-path-in-binary-matrix.py
+++ array/shortest-path-in-binary-matrix.py
@@ -1,22 +1,24 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         #bfs approach
-        queue = deque([(0,0,1)])
-        directions = [(0,0),(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,-1),(-1,0),]
         n = len(grid)
 
         if grid[0][0] or grid[-1][-1]:
             return -1
-        
-        while queue:
-            x, y, d = queue.popleft()
+        q = [(0,0,1)]
+        grid[0][0] = 1
 
-            if 0<= x < n and 0 <= y < n and not grid[x][y]:
-                #code here
-                for x, y in directions:
-                    grid[x][y] = 1
-                    queue.append((x, y, d + 1))
-            if x == grid[-1] and y == grid[-1]:
+        for i, j, d in q:
+            if i == n-1 and j == n-1:
                 return d
-        return -1 
+            directions = [
+                (i -1, j-1),(i-1, j),(i-1, j+1),(i, j-1),
+                (i, j+1),(i+1, j-1),(i+1, j),(i+1, j+1)
+            ]
+
+            for x, y in directions:
+                if 0 <= x < n and 0<= y and <n and not grid[x][y]:
+                    grid[x][y] = 1
+                    q.append(x,y, d+1)
+        return -1
         
