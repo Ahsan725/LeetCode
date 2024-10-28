a51de8a941053710c26a530368d64c2f83d5e8a8
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-10-27 20:43:57 -0400

    LC-minimum-remove-to-make-valid-parentheses [Runtime: 43 ms; Memory: 17.9 MB]

--- stack/minimum-remove-to-make-valid-parentheses.py
+++ stack/minimum-remove-to-make-valid-parentheses.py
@@ -1,23 +1,40 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        res = []
-        count = 0
+        # res = []
+        # count = 0
 
-        for c in s:
+        # for c in s:
+        #     if c == '(':
+        #         res.append(c)
+        #         count += 1
+        #     elif c == ')' and count > 0:
+        #         res.append(c)
+        #         count -= 1
+        #     elif c != ')':
+        #         res.append(c)
+        # result = []
+        # for c in res[::-1]:
+        #     if c == '(' and count > 0:
+        #         count -= 1
+        #     else:
+        #         result.append(c)
+        # return "".join(result[::-1])
+        #below is the stack solution
+        stack = []
+        to_remove = set()
+
+        for i, c in enumerate(s):
             if c == '(':
+                stack.append(i)
+            if c == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    to_remove.add(i)
+        to_remove.update(stack)
+        res = []
+        for i, c in enumerate(s):
+            if i not in to_remove:
                 res.append(c)
-                count += 1
-            elif c == ')' and count > 0:
-                res.append(c)
-                count -= 1
-            elif c != ')':
-                res.append(c)
-        result = []
-        for c in res[::-1]:
-            if c == '(' and count > 0:
-                count -= 1
-            else:
-                result.append(c)
-        return "".join(result[::-1])
-
+        return "".join(res)
 
