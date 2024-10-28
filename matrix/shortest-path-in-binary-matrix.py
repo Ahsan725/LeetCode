c5346b47aaa58928e497b73ad2ef6d34f4ab31af
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-10-28 15:47:58 -0400

    LC-shortest-path-in-binary-matrix [Runtime: N/A; Memory: N/A]

---
+++ matrix/shortest-path-in-binary-matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        #bfs approach
+        queue = deque([(0,0,1)])
+        directions = [(0,0),(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,-1),(-1,0),]
+        n = len(grid)
+        
+        while queue:
+            x, y, d = queue.popleft()
+
+            if 0<= x < n and 0 <= y < n and not grid[x][y]:
+                #code here
+                for x, y in directions:
+                    grid[x][y] = 1
+                    queue.append((x, y, d + 1))
+            if x == grid[-1] and y == grid[-1]:
+                return d
+        return -1 
+        
