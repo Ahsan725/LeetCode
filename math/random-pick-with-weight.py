466de614fa7f0cb854a1eaeac7f6cac2827ebc75
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-10-28 13:15:59 -0400

    LC-random-pick-with-weight [Runtime: 59 ms; Memory: 22 MB]

--- math/random-pick-with-weight.py
+++ math/random-pick-with-weight.py
@@ -16,14 +16,16 @@ class Solution:
         l = 0
         r = len(self.prefix_sums)
 
-        while l < r:
-            mid = (l + r) // 2
+        # while l < r:
+        #     mid = (l + r) // 2
 
-            if self.prefix_sums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid
-        return l
+        #     if self.prefix_sums[mid] < target:
+        #         l = mid + 1
+        #     else:
+        #         r = mid
+
+
+        return bisect_left(self.prefix_sums, target)
         
 
 
