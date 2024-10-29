124be0d79fe178c3f6e632b9be402d61b5e4e1af
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-10-29 14:21:38 -0400

    LC-kth-largest-element-in-an-array [Runtime: 65 ms; Memory: 27.8 MB]

--- quickselect/kth-largest-element-in-an-array.py
+++ quickselect/kth-largest-element-in-an-array.py
@@ -1,29 +1,43 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        if k > 500: #for super large ks since in the worst case the algo below will be N^2 but N in average case. 
-            nums.sort()
-            return nums[-k]
+        heap = []
 
-        k = len(nums) - k
-        
-        def quickselect(l,r):
-            
-            pivot = nums[r]
-            p = l
-
-            #nums = [3,2,1,5,6,4]
-            #.      ip         v
-            for i in range(l, r):
-                if nums[i] < pivot:
-                    #swap
-                    nums[i] , nums[p] = nums[p], nums[i]
-                    p += 1
-            nums[p], nums[r] = nums[r], nums[p]
-
-            if k < p:
-                return quickselect(l,p -1)
-            elif k > p:
-                return quickselect(p + 1, r)
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
             else:
-                return nums[p]
-        return quickselect(0,len(nums)-1)
+                if num > heap[0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, num)
+        return heap[0]
+        
+        
+        
+        # if k > 500: #for super large ks since in the worst case the algo below will be N^2 but N in average case. 
+        #     nums.sort()
+        #     return nums[-k]
+
+        # k = len(nums) - k
+        
+        # def quickselect(l,r):
+            
+        #     pivot = nums[r]
+        #     p = l
+
+        #     #nums = [3,2,1,5,6,4]
+        #     #.      ip         v
+        #     for i in range(l, r):
+        #         if nums[i] < pivot:
+        #             #swap
+        #             nums[i] , nums[p] = nums[p], nums[i]
+        #             p += 1
+        #     nums[p], nums[r] = nums[r], nums[p]
+
+        #     if k < p:
+        #         return quickselect(l,p -1)
+        #     elif k > p:
+        #         return quickselect(p + 1, r)
+        #     else:
+        #         return nums[p]
+        # return quickselect(0,len(nums)-1)
+        
