20926467f97d8a95814418167a9519248775e31c
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-01 13:57:51 -0400

    LC-copy-list-with-random-pointer [Runtime: 36 ms; Memory: 17.5 MB]

---
+++ hash-table/copy-list-with-random-pointer.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        #basically we are copying the linkedlist in a way that all the connections translate over to the new linked list
+        #has two pointers next and random 
+        #iterate over each node and get two values
+       
+        oldtonew = {None : None}
+
+        cur = head
+        while cur:
+            newnode = Node(cur.val)
+            oldtonew[cur] = newnode
+            cur = cur.next
+
+        cur = head #{old : new}
+        while cur:
+            newnode = oldtonew[cur]
+            newnode.next = oldtonew[cur.next]
+            newnode.random = oldtonew[cur.random]
+            cur = cur.next
+
+        return oldtonew[head]
