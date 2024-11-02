4cb601d2379c2eb43b0a5d30925b00cda534a9e1
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-02 13:58:43 -0400

    LC-valid-parentheses [Runtime: 0 ms; Memory: 16.7 MB]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -14,15 +14,16 @@ class Solution:
         #else False
 
         stack = []
-        close_to_open = {")":"(", "}":"{", "]":"["}
+        close_to_open ={"}":"{", "]":"[", ")":"("}
 
         for char in s:
-            if char in close_to_open:
+            if char in close_to_open: #this just means that it is a closing bracket
                 if stack and stack[-1] == close_to_open[char]:
-                    stack.pop()
+                    stack.pop() #basicaly means it closed one of the brackets
                 else:
                     return False
             else:
                 stack.append(char)
-        
-        return True if not stack else False 
+            
+        return True if not stack else False
+
