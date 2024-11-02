35978d2a0555cc99c0a476c3e13373878b48e808
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-02 13:38:54 -0400

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -1,17 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        #I am thinking of using a stack because of the nature f the problem. Where we are going back to look at the
+        #last added element or bracket in this case. And see if that is closing the current one 
+
+        #stack = []
+        # "({[" add this to the stack and then we will check when we encounter the first closng para
+
+        #([{]}) -> stack =["(", "[", "{", ""]
+        #stack.pop() -> 
+
+        #if not stack:
+        #return True
+        #else False
+
         stack = []
 
         for char in s:
-            if char in "([{":
+            if char in "({[":
                 stack.append(char)
-            else:
-                if not stack:
-                    return False
-                top = stack.pop()
-
-                if char == ')' and top != '(' or char == ']' and top != '[' or char == '}' and top != '{':
-                    return False      
-        return len(stack) == 0
-
-
+            if char in ")]}":
+                if char == ')' and stack[-1] == "(":
+                    stack.pop()
+                if char == '}' and stack[-1] == "{":
+                    stack.pop()
+                if char == ']' and stack[-1] == "[":
+                    stack.pop()
+        
+        if not stack:
+            return True
+        else:
+            return False
