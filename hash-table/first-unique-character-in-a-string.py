6806ea57aacb3f868dcd0953fe482949d2fbf4f6
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-05 17:21:08 -0500

    LC-first-unique-character-in-a-string [Runtime: 61 ms; Memory: 16.8 MB]

--- hash-table/first-unique-character-in-a-string.py
+++ hash-table/first-unique-character-in-a-string.py
@@ -5,7 +5,6 @@ class Solution:
         for index, char in enumerate(s):
             freq_map[char] = freq_map.get(char, 0) + 1
 
-
         for char, freq in freq_map.items():
             if freq == 1:
                 return s.index(char)
