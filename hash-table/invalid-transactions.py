0583bce01a23467289d24170d32f4d27c1f34eec
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-05 18:33:36 -0500

    LC-invalid-transactions [Runtime: N/A; Memory: N/A]

--- hash-table/invalid-transactions.py
+++ hash-table/invalid-transactions.py
@@ -22,9 +22,14 @@ class Solution:
             if name in name_to_info: #name is the same
                 prev_time = name_to_info[name]["time"]
                 prev_city = name_to_info[name]["city"]
-                if (abs(prev_time - mins) < 60 and prev_city != city and amount < 1000) or (abs(prev_time - mins) < 60 and prev_city == city and amount < 1000):
+                if abs(prev_time - mins) < 60 and prev_city != city and amount < 1000:
                     invalid.append(trans)
                     invalid.append(full_info[name])
+                elif abs(prev_time - mins) < 60 and prev_city == city and amount < 1000:
+                    if trans and full_info[name] not in invalid:
+                        invalid.append(trans)
+                        invalid.append(full_info[name])
+
 
             name_to_info[name] = {"time" : mins, "city" : city, "amount" : amount}
             full_info[name] = trans # value is the whole string of transaction
