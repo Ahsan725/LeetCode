a322d846c89c3ed5d9c852234bf76c86446761f7
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-05 20:00:32 -0500

    LC-delete-node-in-a-linked-list [Runtime: 40 ms; Memory: 16.9 MB]

--- linked-list/delete-node-in-a-linked-list.py
+++ linked-list/delete-node-in-a-linked-list.py
@@ -11,6 +11,12 @@ class Solution:
         :rtype: void Do not return anything, modify node in-place instead.
         """
 
+        #you are basically taking the current node and changing its value to the value of its next node
+        #then you make this current node point to the next of the next node
+        #essentially you are not deleting this node you are making this node a copy of the next node
+        #and removing the pointer to next node thus essentially deleting it
+    
+
         cur_node = node
         cur_node.val = cur_node.next.val
         cur_node.next = cur_node.next.next
