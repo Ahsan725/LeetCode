d580a39dd88d67fde778c25f0a4ba13fee4f7174
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-06 11:55:05 -0500

    LC-add-two-numbers [Runtime: N/A; Memory: N/A]

--- linked-list/add-two-numbers.py
+++ linked-list/add-two-numbers.py
@@ -1,24 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        tail = dummyHead
-        carry = 0
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #approach: iterate over the array and save the two values by adding them reverse them at the end
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+        number = 0
+        number2 = 0
+        while l1:
+            number = (number * 10) + l1.val
+            l1 = l1.next
+        num1_string = str(number)
+        reverse_nums1 = num1_string[::-1]
+        reverse_nums1 = int(reverse_nums1)
+            
 
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
+        while l2:
+            number2 = (number2 * 10) + l2.val
+            l2 = l2.next
+        num2_string = str(number2)
+        reverse_nums2 = num2_string[::-1]
+        reverse_nums2 = int(reverse_nums2)
 
-            newNode = ListNode(digit)
-            tail.next = newNode
-            tail = tail.next
+        sum_of_nums = str(reverse_nums1 + reverse_nums2 )#196
+        reversed_sum = sum_of_nums[::-1] #691
+    
+        #98 + 98 = 196 -> 8->9 8->9  === 89 then we reversed it so it became 98 + 98 -> 196 but we need ll -> 691
+        #243 564 -> 342 + 465 = 807 -> 708 
+        dummy_head = ListNode(0)
+        current = dummy_head
+        for digit in reversed_sum:
+            current.next = ListNode(int(digit))
+            current = current.next
 
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+        return dummy_head.next
 
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+    
