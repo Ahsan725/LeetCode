63f559d92dae740346b52b48b7b5190a27227637
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-09 00:45:46 -0500

    LC-climbing-stairs [Runtime: 0 ms; Memory: 16.5 MB]

--- dynamic-programming/climbing-stairs.py
+++ dynamic-programming/climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         one, two = 1, 1
 
-        for i in range(n -1):
+        for i in range(n -1):  #bottom up dp approach 
             temp = one
             one = one + two
             two = temp
