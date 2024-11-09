08244bd77e5c1fd1ed2a72a7d07fc7dedd7058c4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-09 15:42:50 -0500

    LC-fibonacci-number [Runtime: 329 ms; Memory: 16.5 MB]

--- dynamic-programming/fibonacci-number.py
+++ dynamic-programming/fibonacci-number.py
@@ -3,4 +3,8 @@ class Solution:
         if n <= 1:
             return n
         return self.fib(n-1) + self.fib(n -2)
+
+        #Time: 0(n^2)
+        #Space; O(n) -> recursion stack 
+        
         
