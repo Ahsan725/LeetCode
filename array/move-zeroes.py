853e4ce4a6fcad9ccf9cf84a0060cd921ca9117a
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-11 19:51:15 -0500

    LC-move-zeroes [Runtime: 6 ms; Memory: 17.7 MB]

--- array/move-zeroes.py
+++ array/move-zeroes.py
@@ -2,11 +2,11 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         j = 0
         for i in range(len(nums)):
-            if nums[i] == 0:
-                if nums[j] != 0:
-                    j = i 
+            # if nums[i] == 0:
+            #     if nums[j] != 0:
+            #         j = i 
 
-            if nums[i] != 0 and nums[j] == 0:
+            if nums[i] != 0:
                 nums[j],nums[i] = nums[i], nums[j]
                 j += 1
         return nums
