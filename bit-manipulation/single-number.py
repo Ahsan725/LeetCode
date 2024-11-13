e8272948ed422d7ebca735b47841af6f1001c7c0
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-12 23:28:14 -0500

    LC-single-number [Runtime: 1 ms; Memory: 18.5 MB]

--- bit-manipulation/single-number.py
+++ bit-manipulation/single-number.py
@@ -1,13 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        newmap = {}
-        for i in range(len(nums)):
-            newmap[nums[i]] = newmap.get(nums[i], 0) + 1
-        
-        for key, value in newmap.items():
-            if newmap[key] <= 1:
-                return key
-        return -1
+        xor_result = 0
+        for num in nums:
+            xor_result ^= num  # XOR each element with xor_result
+        return xor_result
+
         
 
         
