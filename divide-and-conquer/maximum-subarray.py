1c7619fa071e850fb583d8d70ce750bc06ff81ed
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-11-13 16:59:53 -0500

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/maximum-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        summ = 0
+        maxsum = float('-inf')
+        for i in range(len(nums)):
+
+            summ += nums[i]
+            maxsum = max(maxsum, summ)
+        
+            if not summ > 0:
+                summ = 0
+        return maxsum if maxsum > 0 else []
