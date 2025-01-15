ff2c6528fbba781565c39b846f926934f44d3d47
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-01-15 16:45:57 -0500

    LC-add-two-numbers [Runtime: 0 ms; Memory: 17.9 MB]

--- math/add-two-numbers.py
+++ math/add-two-numbers.py
@@ -5,36 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #approach: iterate over the array and save the two values by adding them reverse them at the end
+        dummy = ListNode()
+        curr = dummy 
 
-        number = 0
-        number2 = 0
-        while l1:
-            number = (number * 10) + l1.val
-            l1 = l1.next
-        num1_string = str(number)
-        reverse_nums1 = num1_string[::-1]
-        reverse_nums1 = int(reverse_nums1)
-            
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-        while l2:
-            number2 = (number2 * 10) + l2.val
-            l2 = l2.next
-        num2_string = str(number2)
-        reverse_nums2 = num2_string[::-1]
-        reverse_nums2 = int(reverse_nums2)
+            #new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
 
-        sum_of_nums = str(reverse_nums1 + reverse_nums2 )#196
-        reversed_sum = sum_of_nums[::-1] #691
-    
-        #98 + 98 = 196 -> 8->9 8->9  === 89 then we reversed it so it became 98 + 98 -> 196 but we need ll -> 691
-        #243 564 -> 342 + 465 = 807 -> 708 
-        dummy_head = ListNode(0)
-        current = dummy_head
-        for digit in reversed_sum:
-            current.next = ListNode(int(digit))
-            current = current.next
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-        return dummy_head.next
-
-    
+        return dummy.next
