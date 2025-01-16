104f7c4c971d9542df8f8be7ff4c50487df186da
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-01-16 18:50:43 -0500

    LC-best-sightseeing-pair [Runtime: N/A; Memory: N/A]

--- array/best-sightseeing-pair.py
+++ array/best-sightseeing-pair.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         res = []
+        biggest = -999
 
         def calculate_sight(i,j):
             return values[i] + values[j] + i - j
 
         for i in range(len(values)-1):
             for j in range(i + 1, len(values)):
-                res.append(calculate_sight(i,j))
-
-
-        sorted_res = sorted(res)
+                pair = calculate_sight(i,j)
+                if pair > biggest:
+                    biggest = pair
         
-        return sorted_res[-1]
+        return biggest
