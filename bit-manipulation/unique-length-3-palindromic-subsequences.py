1f971703d59b62e0ca4eacdb7c89522567de96b9
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-01-17 16:18:09 -0500

    LC-unique-length-3-palindromic-subsequences [Runtime: N/A; Memory: N/A]

--- bit-manipulation/unique-length-3-palindromic-subsequences.py
+++ bit-manipulation/unique-length-3-palindromic-subsequences.py
@@ -1,12 +1,14 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        res = 0
-        uniq = set(s)
+        res = 0 #this is what we will use to count the number of palins 
+        uniq = set(s) #create a set of characters in s 
 
-        for c in uniq:
+        for c in s: 
             start = s.find(c)
             end = s.rfind(c)
 
             if start < end:
                 res += len(set(s[start + 1: end]))
         return res
+
+        #better than neetcode solution 
