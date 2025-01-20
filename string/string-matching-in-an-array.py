b6568113020bf7436dd055dd159ced1e6647e5b9
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-01-20 15:16:00 -0500

    LC-string-matching-in-an-array [Runtime: 3 ms; Memory: 17.9 MB]

---
+++ string/string-matching-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res = set()
+        n = len(words)
+        
+        # Iterate through all pairs of words
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    if words[i] in words[j]:
+                        res.add(words[i])
+                        break  # No need to check other words once a match is found
+        
+        return list(res)
