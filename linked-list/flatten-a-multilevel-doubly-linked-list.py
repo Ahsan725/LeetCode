656a14b373f9e4d0853acf0ba70e4a742df0317c
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-01-20 20:59:37 -0500

    LC-flatten-a-multilevel-doubly-linked-list [Runtime: 49 ms; Memory: 18.3 MB]

---
+++ linked-list/flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        dummy = Node()
+        cur = dummy
+        stack = [head]
+
+        while stack:
+            temp = stack.pop()
+            if temp.next:
+                stack.append(temp.next)
+            if temp.child:
+                stack.append(temp.child)
+            cur.next = temp
+            temp.prev = cur
+            temp.child = None
+            cur = temp
+        dummy.next.prev = None
+        return dummy.next
