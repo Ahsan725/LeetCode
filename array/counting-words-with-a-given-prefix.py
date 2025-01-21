a193cb7a5f16d8867f83566d0fd3995a62bac4cf
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-01-21 17:56:34 -0500

    LC-counting-words-with-a-given-prefix [Runtime: 0 ms; Memory: 18 MB]

---
+++ array/counting-words-with-a-given-prefix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        res = 0
+        for i in range(len(words)):
+            if words[i].startswith(pref):
+                res += 1
+        
+        return res
