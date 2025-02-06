ad6e7d6707ca41aa00951d8521675b4a858f45f4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-05 22:40:56 -0500

    LC-palindrome-number [Runtime: 8 ms; Memory: 17.7 MB]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,8 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        
         if x < 0:
             return False
-        
-        x = str(x)
-        
-        return x == x[::-1]
+        #because if there is a negative sign on one side it can never be palins
+        original = x
+        reverse = 0 
+        while x > 0:
+            last = x % 10 #786 -> 6 786%10-> 780 -> 6 
+            reverse = (reverse * 10) + last
+            x = x // 10 # 786 // 10 -> 78
+        return original == reverse 
