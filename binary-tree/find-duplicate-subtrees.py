3e1f464086301d242f2ce88f8da762e9bb52513e
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-05 20:30:20 -0500

    LC-find-duplicate-subtrees [Runtime: 9 ms; Memory: 25 MB]

--- binary-tree/find-duplicate-subtrees.py
+++ binary-tree/find-duplicate-subtrees.py
@@ -7,18 +7,17 @@
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
         subtrees = defaultdict(list)
-
+        #create a string representation of the tree which can be used as a hashable key in the hashmap
+        #
         def dfs(node):
             if not node:
-                return "#"
-            
+                return "#" #could be anything like null
             s = ",".join([str(node.val), dfs(node.left), dfs(node.right)])
-
-            if len(subtrees[s]) == 1:
-                res.append(node)
-            subtrees[s].append(node)
+            if len(subtrees[s]) == 1: #if there was already a tree like this in the list of the hashmap with key s. 
+            #we also only want to add it once in the res that's why it is set to == 1 
+                res.append(node) #we found a duplicate so add to the res
+            subtrees[s].append(node) #reagrdless we should add it to the hashmap 
             return s
-
 
         res = []
         dfs(root)
