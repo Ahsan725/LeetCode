974e2efa50fb7adb1776111fbc257e6c13fe2dcb
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-07 15:58:39 -0500

    LC-two-sum-ii-input-array-is-sorted [Runtime: 4 ms; Memory: 18.5 MB]

--- array/two-sum-ii-input-array-is-sorted.py
+++ array/two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        res = []
 
         l, r = 0, len(numbers) - 1
 
@@ -13,7 +12,5 @@ class Solution:
                 #too small increase l
                 l += 1
             else:
-                res.append(l+1)
-                res.append(r+1)
-                break
-        return res
+                return [l+1, r+1]
+        return []
