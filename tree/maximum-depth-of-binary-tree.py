372c0b9f7064522284904a9c04b01ba6dc856092
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-10 18:50:03 -0500

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        #uss bfs maybe for level order traversal?
+
+        q = deque([root])
+        level = 0
+        while q:
+            for _ in range(len(q)):
+                current = q.popleft()
+                if current.left:
+                    q.append(current.left)
+                if current.right:
+                    q.append(current.right)
+            level += 1
+
+        return level 
+        
