3f32d352de3d83370daf5651bf5fa108439b88fc
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-13 13:58:50 -0500

    LC-find-the-duplicate-number [Runtime: 27 ms; Memory: 30.1 MB]

--- binary-search/find-the-duplicate-number.py
+++ binary-search/find-the-duplicate-number.py
@@ -3,8 +3,20 @@ class Solution:
         #we can not use extra space 
         #can not modify -> no sorting 
 
-        for num in nums:
-            for num2 in nums:
-                if num == num2:
-                    return num
-        return 0
+        fast = 0 
+        slow = 0
+
+        while True:
+
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        fast = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+            
+        return slow
+            
