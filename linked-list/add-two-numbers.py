a3680c962a856fe044f0b19cd1f85cf2d66c1d51
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-13 13:19:58 -0500

    LC-add-two-numbers [Runtime: 4 ms; Memory: 18 MB]

--- linked-list/add-two-numbers.py
+++ linked-list/add-two-numbers.py
@@ -5,22 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        curr = dummy 
-
+        dummy = ListNode(0)
+        cur = dummy 
+        
         carry = 0
         while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-
-            #new digit
-            val = v1 + v2 + carry
-            carry = val // 10
-            val = val % 10
-            curr.next = ListNode(val)
-
-            curr = curr.next
-            l1 = l1.next if l1 else None
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
+            total = num1 + num2 + carry
+            carry = total // 10
+            value = total % 10
+            cur.next = ListNode(value)
+            
+            cur = cur.next
+            l1 = l1.next if l1 else None 
             l2 = l2.next if l2 else None
             
         return dummy.next
+        
