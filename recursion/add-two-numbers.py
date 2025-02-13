a37ac29f4e2b22686058c30631c8d88701e383ca
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-02-13 13:21:09 -0500

    LC-add-two-numbers [Runtime: 6 ms; Memory: 17.9 MB]

--- recursion/add-two-numbers.py
+++ recursion/add-two-numbers.py
@@ -10,13 +10,16 @@ class Solution:
         
         carry = 0
         while l1 or l2 or carry:
+            #extract the values to add
             num1 = l1.val if l1 else 0
             num2 = l2.val if l2 else 0
+            #calculate the sum and carry and the value to add
             total = num1 + num2 + carry
             carry = total // 10
             value = total % 10
+            #create a new listnode and add the value
             cur.next = ListNode(value)
-            
+            #move all the three of the linked list pointers 
             cur = cur.next
             l1 = l1.next if l1 else None 
             l2 = l2.next if l2 else None
