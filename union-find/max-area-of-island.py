329c988ba1ee349cf97662d0ff05022d03eaffb1
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-14 00:27:50 -0400

    LC-max-area-of-island [Runtime: N/A; Memory: N/A]

---
+++ union-find/max-area-of-island.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        seen = set()
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) == 0 or (r,c) in seen:
+                return
+            seen.add((r,c))
+            return (1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c -1))
+
+        area = 0
+        for r in range(len(rows)):
+            for c in range(len(cols)):
+                area = max(area, dfs(r,c))
+        return area 
