19fee802ca577c3533d66514404634c6709059ed
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-14 02:50:02 -0400

    LC-positions-of-large-groups [Runtime: 5 ms; Memory: 18 MB]

---
+++ string/positions-of-large-groups.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        l = 0
+        r = 0
+        start = 0
+        end = 0
+        res =[]
+
+        while r < len(s):
+            if s[l] == s[r]:
+                start = l
+                while r < len(s) and s[l] == s[r]:
+                    r += 1
+                end = r - 1
+                l = r
+                if end - start >= 2:
+                    res.append([start, end])
+        return res
