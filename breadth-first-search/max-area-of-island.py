3abf2ee38e063da2e04b1bf0b6b4414618ae3903
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-14 00:29:04 -0400

    LC-max-area-of-island [Runtime: N/A; Memory: N/A]

--- breadth-first-search/max-area-of-island.py
+++ breadth-first-search/max-area-of-island.py
@@ -5,7 +5,7 @@ class Solution:
         cols = len(grid[0])
 
         def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) == 0 or (r,c) in seen:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0 or (r,c) in seen:
                 return
             seen.add((r,c))
             return (1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c -1))
