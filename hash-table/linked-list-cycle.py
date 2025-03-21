cb1567a483ca3b64f96b70e741b54736fc01e1f0
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-20 21:21:24 -0400

    LC-linked-list-cycle [Runtime: 50 ms; Memory: 19.8 MB]

--- hash-table/linked-list-cycle.py
+++ hash-table/linked-list-cycle.py
@@ -6,12 +6,27 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
-        while head:
-            if head in seen:
+
+        # #there is a a two ptr approach as well where one pointer is fast and the other is slow.
+        # seen = set()
+        # while head:
+        #     if head in seen:
+        #         return True
+        #     else:
+        #         seen.add(head)
+        #     head = head.next
+        # return False 
+
+        if not head:
+            return None 
+
+        slow = head
+        fast = head 
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            if slow == fast:
                 return True
-            else:
-                seen.add(head)
-            head = head.next
-        return False 
+        return False
         
