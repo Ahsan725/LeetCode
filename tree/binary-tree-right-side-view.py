274f98adfaa83032de5c79cc9e8066b602f10eef
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-28 03:35:10 -0400

    LC-binary-tree-right-side-view [Runtime: 0 ms; Memory: 17.9 MB]

--- tree/binary-tree-right-side-view.py
+++ tree/binary-tree-right-side-view.py
@@ -4,22 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+from typing import Optional, List
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        depth = 0
 
-        def dfs(root, depth):
+        if not root:
+            return res
 
-            if not root:
-                return None
-            
-            if depth == len(res):
-                res.append(root.val)
-        
+        q = deque([root])  # Initialize the queue with the root node
+        while q:
+            level_size = len(q)
+            level_nodes = []  # Store nodes in the current level
 
-            dfs(root.right, depth + 1)
-            dfs(root.left, depth + 1)
-        
-        dfs(root, depth)
+            for i in range(level_size):
+                node = q.popleft()
+                level_nodes.append(node.val) #add the node value to level_nodes
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            if level_nodes: #if the level_nodes list is not empty
+                res.append(level_nodes[-1]) #append the last value of the level to the result array.
+
         return res
