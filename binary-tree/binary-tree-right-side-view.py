e25c456712d7c1cf92fedcb0272d61dea1c9b6d0
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-28 03:37:31 -0400

    LC-binary-tree-right-side-view [Runtime: 0 ms; Memory: 17.6 MB]

--- binary-tree/binary-tree-right-side-view.py
+++ binary-tree/binary-tree-right-side-view.py
@@ -4,9 +4,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-from typing import Optional, List
-
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -14,21 +11,18 @@ class Solution:
         if not root:
             return res
 
-        q = deque([root])  # Initialize the queue with the root node
+        q = deque([root])
         while q:
-            # level_size = len(q)
-            level_nodes = []  # Store nodes in the current level
-
+            level = []
             for i in range(len(q)):
                 node = q.popleft()
-                level_nodes.append(node.val) #add the node value to level_nodes
+                level.append(node.val)
 
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
 
-            if level_nodes: #if the level_nodes list is not empty
-                res.append(level_nodes[-1]) #append the last value of the level to the result array.
-
-        return res
+            res.append(level[-1])
+        return res 
+                
