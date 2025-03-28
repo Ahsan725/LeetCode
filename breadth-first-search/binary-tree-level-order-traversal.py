79004b27af3a9ff6a10b82326790946a24eeb897
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-28 02:57:43 -0400

    LC-binary-tree-level-order-traversal [Runtime: 0 ms; Memory: 18.3 MB]

--- breadth-first-search/binary-tree-level-order-traversal.py
+++ breadth-first-search/binary-tree-level-order-traversal.py
@@ -1,30 +1,27 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        
-        q = deque([root])
         res = []
-
-        while q:
-            level_size = len(q)  # Number of nodes at the current level this so we can run the loop this many times
-            level_nodes = []  # Stores node values for this level since we are returning a list of lists
-
-            for _ in range(level_size):
-                current = q.popleft()
-                level_nodes.append(current.val)  # Store value, not node
-                
-                if current.left:
-                    q.append(current.left)
-                if current.right:
-                    q.append(current.right)
-            
-            res.append(level_nodes)  # Append the entire level to result
         
+        if not root:
+            return res
+
+        q = deque([root])
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            res.append(level)
+            
         return res
