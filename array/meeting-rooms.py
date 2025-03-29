fcb08d185ca4cd5b595162aadc5fe09c80765ff4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-29 03:39:34 -0400

    LC-meeting-rooms [Runtime: 11 ms; Memory: 20 MB]

--- array/meeting-rooms.py
+++ array/meeting-rooms.py
@@ -5,16 +5,18 @@ class Solution:
         # [[7,10],[2,4]] -> [2,4] [7,10]
 
         intervals.sort(key=lambda x : x)
+        print(intervals)
 
         for i in range(1, len(intervals)):
-            start = intervals[0][0]
-            end = intervals[0][1]
+            start = intervals[i -1][0]
+            end = intervals[i - 1][1]
 
             newstart = intervals[i][0]
             newend = intervals[i][0]
 
             if newstart < end:
                 return False
+        
         return True
                 
 
