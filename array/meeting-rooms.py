86e87fbcc423cc515c1c85c5698b4d50eb40d7ce
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-03-29 03:42:20 -0400

    LC-meeting-rooms [Runtime: 4 ms; Memory: 19.8 MB]

--- array/meeting-rooms.py
+++ array/meeting-rooms.py
@@ -1,15 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        #[[0,30],[5,10],[15,20]]
-        #if we sort by start
-        # [[7,10],[2,4]] -> [2,4] [7,10]
-
+        #sorting is necessary
         intervals.sort(key=lambda x : x)
 
         for i in range(1, len(intervals)):
-            
+            #get previous element end time
             prevend = intervals[i - 1][1]
-
+            #get new meetings start time 
             newstart = intervals[i][0]
 
             if newstart < prevend:
