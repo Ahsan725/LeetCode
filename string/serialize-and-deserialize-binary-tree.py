b8fb0fce0d88e7a88a9b26fc404b71f09b84709d
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-01 18:02:03 -0400

    LC-serialize-and-deserialize-binary-tree [Runtime: N/A; Memory: N/A]

--- string/serialize-and-deserialize-binary-tree.py
+++ string/serialize-and-deserialize-binary-tree.py
@@ -41,7 +41,7 @@ class Codec:
         values = data.split(",")
         self.idx = 0
 
-        def build(values)
+        def build(values):
         #base case
         if values[self.idx] == "#":
             self.idx += 1
