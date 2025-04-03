4f2a229264415467bebd6345cbfaf8acb19d514f
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-02 23:57:14 -0400

    LC-balanced-binary-tree [Runtime: 3 ms; Memory: 18.9 MB]

--- tree/balanced-binary-tree.py
+++ tree/balanced-binary-tree.py
@@ -13,11 +13,13 @@ class Solution:
             if not root:
                 return [True, 0] #return true  = balanced and height of 0
 
-            left = dfs(root.left)
-            right = dfs(root.right)
-
+            left = dfs(root.left) #this will get the height and check if balanced
+            right = dfs(root.right) #same for right 
+            
+            #check if the balanced variable will be True or False for curr Node
             balanced = left[0] and right [0] and (abs(left[1] - right[1]) <= 1)
 
+            #return true or false and the height of the tree until this node
             return [balanced, 1 + max(left[1], right[1])]
-
+        
         return dfs(root)[0]
