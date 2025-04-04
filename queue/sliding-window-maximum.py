84bfbdb1a0d7769fd1f5116dae230cc6223c9aa3
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-04 08:47:15 -0400

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

---
+++ queue/sliding-window-maximum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        prevMax = float('-inf')
+
+        l = 0
+        r = k - 1 
+
+        while r < len(nums):
+            for i in range(l, r+1):
+                prevMax = max(prevMax, nums[i])
+            res.append(prevMax)
+            l += 1
+            r += 1
+        return res  
