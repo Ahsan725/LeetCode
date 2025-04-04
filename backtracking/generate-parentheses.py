32a8a3a2818b64b746f8a9dd43853467c5f6bde4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-04 12:38:51 -0400

    LC-generate-parentheses [Runtime: 0 ms; Memory: 18.2 MB]

--- backtracking/generate-parentheses.py
+++ backtracking/generate-parentheses.py
@@ -8,7 +8,7 @@ class Solution:
         # strsofar: current parentheses string being built
         # numofopen: number of '(' used so far
         # numofclose: number of ')' used so far
-        def addparen(strsofar: str, numofopen: int, numofclose: int):
+        def addparen(strsofar, numofopen, numofclose):
             # Base case: if the current string has 2 * n characters, it's complete
             if len(strsofar) == 2 * n:
                 self.res.append(strsofar)
@@ -22,6 +22,6 @@ class Solution:
             if numofclose < numofopen:
                 addparen(strsofar + ')', numofopen, numofclose + 1)
 
-        # Start with an empty string and zero open/close parentheses used
+        # Start with an empty string and zero open/close parentheses used or first open paren
         addparen('(', 1, 0)
         return self.res
