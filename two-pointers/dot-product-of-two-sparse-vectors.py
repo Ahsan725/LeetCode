b4a4ae9e6a63d2a4aa438a3cf0d0e4b399da9ebb
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-05 21:07:57 -0400

    LC-dot-product-of-two-sparse-vectors [Runtime: 1306 ms; Memory: 21.6 MB]

--- two-pointers/dot-product-of-two-sparse-vectors.py
+++ two-pointers/dot-product-of-two-sparse-vectors.py
@@ -1,14 +1,20 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.data = {i: num for i, num in enumerate(nums) if num != 0}
+        self.data = {}
+        for i, num in enumerate(nums):
+            if num:
+                self.data[i] = num
 
+    # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
-        # Iterate over the smaller dict for efficiency
-        if len(self.data) > len(vec.data):
-            return vec.dotProduct(self)
+        out, vec2 = 0, vec.data
+        for key, val in self.data.items():
+            if key in vec2:
+                out+=vec2[key]*val
+        return out
+        
 
-        result = 0
-        for i, val in self.data.items():
-            if i in vec.data:
-                result += val * vec.data[i]
-        return result
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
