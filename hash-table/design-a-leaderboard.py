58d48815c94c1b680f5dfa169eaef0943c4ad6a4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-07 14:19:54 -0400

    LC-design-a-leaderboard [Runtime: 23 ms; Memory: 18.5 MB]

---
+++ hash-table/design-a-leaderboard.py
@@ -0,0 +1,41 @@
+#Understanding: we have to create a leaderboard and implement some functions. some observatrions I have made are that
+#we are adding at the end, when we call reset it removes from the leaderboard. 
+#Approach: since we are appending at the end we can use a list and add tuples to represent the id and the score
+#addscore -> O(1),
+#reset -> Map key ?  use a list to make topk efficent a heap and maintain a heap of n values (worst case) 
+#Main = Map {key:id, value:score}, Heap(tuple(id, score)) -> maxheap -(9) 73->56 pid:1 adscore(1,23), addscore(1,20)
+#better? map -> top iteratve over the map and create a maxheap of k 
+class Leaderboard:
+
+    def __init__(self):
+        self.leader = {}
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId in self.leader:
+            #we update
+            self.leader[playerId] += score
+        else:
+            #we add
+            self.leader[playerId] = score
+
+    def top(self, K: int) -> int:
+        sumscore = 0
+        scores = [-val for val in list(self.leader.values())]
+        heapq.heapify(scores) #create a max heap
+        for i in range(K):
+            value = heapq.heappop(scores)
+            sumscore += (value * -1) #negating to return to original value
+        return sumscore
+
+    def reset(self, playerId: int) -> None:
+        if playerId in self.leader:
+            #we remove
+            del self.leader[playerId]
+
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
