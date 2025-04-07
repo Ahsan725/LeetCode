df57a4d38c06dd32ccc1c425e016024ace6b22a7
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-07 14:26:34 -0400

    LC-design-a-leaderboard [Runtime: 19 ms; Memory: 18.3 MB]

--- sorting/design-a-leaderboard.py
+++ sorting/design-a-leaderboard.py
@@ -10,7 +10,7 @@ class Leaderboard:
     def __init__(self):
         self.leader = {}
 
-    def addScore(self, playerId: int, score: int) -> None:
+    def addScore(self, playerId: int, score: int) -> None: #O(1)
         if playerId in self.leader:
             #we update
             self.leader[playerId] += score
@@ -18,21 +18,22 @@ class Leaderboard:
             #we add
             self.leader[playerId] = score
 
-    def top(self, K: int) -> int:
+    def top(self, K: int) -> int: #O(nlogk) where k <= to n O(k)
         sumscore = 0
-        scores = [-val for val in list(self.leader.values())]
-        heapq.heapify(scores) #create a max heap
+        scores = [-val for val in list(self.leader.values())] #syntax review 
+        heapq.heapify(scores) #create a max heap #syntax review
         for i in range(K):
             value = heapq.heappop(scores)
             sumscore += (value * -1) #negating to return to original value
         return sumscore
 
-    def reset(self, playerId: int) -> None:
+    def reset(self, playerId: int) -> None: #O(1)
         if playerId in self.leader:
             #we remove
             del self.leader[playerId]
 
-
+#feedback: everytime you see topk -> always use the heap. Global vs when method is called? -> if the values that you are creating a heap for are changing dynamically then there is n o point to create heap early
+#Critical Feedback: always wanna use tuple for no reason. keep the key value as simple / primitive as possible. Only do it when absoluetly necessary. 
 
 # Your Leaderboard object will be instantiated and called as such:
 # obj = Leaderboard()
