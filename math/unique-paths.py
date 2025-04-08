6297461214bf97803b9c59927bb61eab8704e9fe
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-08 00:07:38 -0400

    LC-unique-paths [Runtime: 3 ms; Memory: 18.1 MB]

--- math/unique-paths.py
+++ math/unique-paths.py
@@ -1,15 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-
+        memo = {(0,0) : 1}
         def paths(i, j):
-
-            #bas case
-            if i == 0 and j == 0:
-                return 1
-            elif i < 0 or j < 0 or i >= m and j >= n:
+            #base case
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if i < 0 or j < 0 or i >= m and j >= n:
                 return 0
             else:
-                return paths(i, j - 1)  + paths(i - 1, j)
+                val =  paths(i, j - 1)  + paths(i - 1, j)
+                memo[(i, j)] = val
+                return val
         
         return paths(m-1, n-1) #starting from the end 
