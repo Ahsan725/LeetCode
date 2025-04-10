41bbaeaa3207f2aadb083f8d8cd4874f2eb21e5e
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-10 12:33:29 -0400

    LC-search-a-2d-matrix [Runtime: 0 ms; Memory: 18.5 MB]

--- array/search-a-2d-matrix.py
+++ array/search-a-2d-matrix.py
@@ -1,30 +1,23 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #def binary searech
-        #this solution treats the 2d matrix as a 1d list. it picks the middle index. This index is then converted into the cordinate of the 2d matrix
-        #this is done using division by cols because what is in 2d matrix that is not present in a 1d list -> cols. 
-
         rows = len(matrix)
         cols = len(matrix[0])
-        total = rows * cols
+        t = rows * cols
         l = 0
-        r = total - 1 
-
-
+        r = t -1 
+        
         while l <= r:
-            mid = (l+r) //2
+            mid = (l + r) // 2
+            #the observation below is the key to solving
             i = mid // cols
-            j = mid % cols 
-
+            j = mid % cols
+            
             if matrix[i][j] == target:
-                return True 
+                return True
             elif matrix[i][j] < target:
                 l = mid + 1
             else:
-                r = mid -1 
-
+                r = mid - 1 
+                
         return False
-
-    
-
         
