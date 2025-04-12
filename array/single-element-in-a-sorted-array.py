f73a93821de3becb5b563de1990aa0f74eabda7a
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-12 04:36:29 -0400

    LC-single-element-in-a-sorted-array [Runtime: 0 ms; Memory: 24.9 MB]

--- array/single-element-in-a-sorted-array.py
+++ array/single-element-in-a-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         lo = 0
         hi = len(nums) - 1
         while lo < hi:
+            # mid = lo + (hi - lo) // 2
             mid = lo + (hi - lo) // 2
             if mid % 2 == 1:
                 mid -= 1
