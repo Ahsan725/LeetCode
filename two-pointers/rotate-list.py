a4538fe5a54d87dae79d44ed0e2949a019a8e515
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-14 14:48:49 -0400

    LC-rotate-list [Runtime: 0 ms; Memory: 18 MB]

--- two-pointers/rotate-list.py
+++ two-pointers/rotate-list.py
@@ -8,6 +8,10 @@ class Solution:
         if not head or not head.next or k == 0:
             return head
 
+        #[to solve this problem we need 4 variables: new tail, new head, org_head, org_tail
+        #then we do this new_tail -> None, org_tail -> org_head, return new_head
+        #this is the entire solution rest is basically how do we find these node positions]
+
         # Step 1: Compute the length of the list
         length = 1
         tail = head
@@ -15,12 +19,13 @@ class Solution:
             tail = tail.next
             length += 1
 
-        # Step 2: Normalize k (in case it's >= length)
+        # Step 2: Normalize k (in case it's >= length) (e.g k = 17 this is the same as rotating by 2 if len is 5)
         k = k % length
         if k == 0:
             return head
 
         # Step 3: Find the new tail node (at index length - k - 1)
+        #new tail is basically the node where we will cut the list
         new_tail = head
         for _ in range(length - k - 1):
             new_tail = new_tail.next
