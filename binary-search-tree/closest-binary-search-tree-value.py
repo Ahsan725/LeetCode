c63c48127b435d1a0eeeab2052ee2c3c09b5ad98
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-16 17:17:15 -0400

    LC-closest-binary-search-tree-value [Runtime: N/A; Memory: N/A]

--- binary-search-tree/closest-binary-search-tree-value.py
+++ binary-search-tree/closest-binary-search-tree-value.py
@@ -5,28 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def closestVa# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
-class Solution:
-    def closestValue(self, root: TreeNode, target: float) -> int:
-        c = root.val
-        while root:
-            # update the closest value once the current note's value is more closet to target:
-            if abs(root.val - target) < abs(c - target):
-                c = root.val
-            # use the binary search tree's property to find the closest value to target
-            else:
-                if root.val < target:
-                    root = root.right
-                else:
-                    root = root.left
-        return calue(self, root: Optional[TreeNode], target: float) -> int:
+    def closestVaalue(self, root: Optional[TreeNode], target: float) -> int:
         
 
         q = deque([root])
