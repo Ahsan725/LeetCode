fd6df3f30faeaeff1e77ebc8b5bedcb5e89ab915
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-16 17:03:46 -0400

    LC-all-nodes-distance-k-in-binary-tree [Runtime: 38 ms; Memory: 18 MB]

--- tree/all-nodes-distance-k-in-binary-tree.py
+++ tree/all-nodes-distance-k-in-binary-tree.py
@@ -1,16 +1,40 @@
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        #create a graph
+        """
+        this problem has two parts create a graph because that will allow us to traverse the parent as well. You use a q and to use bfs and a graph as a default dict
+        if you
+        """
+
+        q = deque([root])
+        graph = defaultdict(list)
+
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+
+                if node.left:
+                    graph[node].append(node.left)
+                    graph[node.left].append(node)
+                    q.append(node.left)
+                if node.right:
+                    graph[node].append(node.right)
+                    graph[node.right].append(node)
+                    q.append(node.right)
+        
         res = []
+        seen = set([target])
 
-        def dfs(node, depth):
-            if not node:
-                return
-            if depth == k:
-                res.append(node.val)
-                return
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
+        q2 = deque([(target, 0)])
+        while q2:
+            for _ in range(len(q2)):
+                node, distance = q2.popleft()
 
-        # Start DFS from target node
-        dfs(target, 0)
-        return res
+                if distance == k:
+                    res.append(node.val)
+                else:
+                    for edge in graph[node]:
+                        if edge not in seen:
+                            seen.add(edge)
+                            q2.append((edge, distance + 1))
+        return res 
