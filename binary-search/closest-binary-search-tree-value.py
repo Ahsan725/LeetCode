13aaf87c798876ef96f211cefd3e8d7d8e69d26d
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-16 17:14:33 -0400

    LC-closest-binary-search-tree-value [Runtime: N/A; Memory: N/A]

--- binary-search/closest-binary-search-tree-value.py
+++ binary-search/closest-binary-search-tree-value.py
@@ -12,7 +12,7 @@ class Solution:
         closest = root.val
 
         while q:
-            for _ in range(len(q)):
+            while q
                 node = q.popleft()
 
                 if abs(node.val - target) < abs(closest - target):
