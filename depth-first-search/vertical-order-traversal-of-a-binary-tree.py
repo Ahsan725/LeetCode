47e16d59c008c8794caa81b78bd011dccb35dbf4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-17 15:02:11 -0400

    LC-vertical-order-traversal-of-a-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        col_map = defaultdict(list)
+        min_col = float('inf')
+        max_col = float('-inf')
+        res = []
+        
+        q = deque([(root, 0, 0)])
+        while q:
+            node, row, col in q.popleft()
+            col_map[col].append(row, node.val)
+            
+            min_col = min(min_col, col)
+            max_col = max(max_col, col)
+            
+            if node.left:
+                q.append(node.left, row - 1, col - 1)
+            if node.right:
+                q.append(node.right, row + 1, col + 1)
+        
+        for level in range(min_col, max_col + 1):
+            items = col_map[level]
+            items.sort()
+            
+            res.append([val for _, val in items])
+        return res
+                        
+        
