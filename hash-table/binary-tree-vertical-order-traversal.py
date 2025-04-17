136f86c6f606402ff4fdc1362d89a44794051540
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-17 14:41:18 -0400

    LC-binary-tree-vertical-order-traversal [Runtime: N/A; Memory: N/A]

--- hash-table/binary-tree-vertical-order-traversal.py
+++ hash-table/binary-tree-vertical-order-traversal.py
@@ -1,32 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-
-        queue = deque([(0, root)])
-        columns = defaultdict(list)
+        #bfs 
         res = []
-        min_x = float('inf')
-        max_x = float('-inf')
-
-        while queue:
-            x, node = queue.popleft()
-            if node:
-                columns[x].append(node.val)
-                min_x = min(min_x, x)
-                max_x = max(max_x, x)
-
-                if node.left:
-                    queue.append((x - 1, node.left))
-                if node.right:
-                    queue.append((x + 1, node.right))
-
-        for level in range(min_x, max_x + 1):
-            res.append(columns[level])
-        return res
+        col_map = deafultdict(list)
+        
+        minx = float('inf')
+        maxx = float('-inf')
+        q = deque([0, root])
+        
+        while q:
+            x, node = q.popleft()
+            
+            col_map[x].append(node.val)
+            minx = min(minx, x)
+            maxx = max(maxx, x)
+            
+            if node.left:
+                q.append(x - 1, node)
+            if node.right:
+                q.append(x + 1, node)
+            
+        for level in range(minx, maxx + 1):
+            res.append(col_map[level])
+        return res 
+            
