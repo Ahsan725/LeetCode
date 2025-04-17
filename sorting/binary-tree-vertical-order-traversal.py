76c59abd7507e1eaa7e00757455f7d95b07654b3
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-17 14:50:56 -0400

    LC-binary-tree-vertical-order-traversal [Runtime: 3 ms; Memory: 17.6 MB]

--- sorting/binary-tree-vertical-order-traversal.py
+++ sorting/binary-tree-vertical-order-traversal.py
@@ -19,16 +19,16 @@ class Solution:
         q = deque([(0, root)])
         
         while q:
-            x, node = q.popleft()
+            col, node = q.popleft()
             
-            col_map[x].append(node.val)
-            minx = min(minx, x)
-            maxx = max(maxx, x)
+            col_map[col].append(node.val)
+            minx = min(minx, col)
+            maxx = max(maxx, col)
             
             if node.left:
-                q.append((x - 1, node.left))
+                q.append((col - 1, node.left))
             if node.right:
-                q.append((x + 1, node.right))
+                q.append((col + 1, node.right))
             
         for level in range(minx, maxx + 1):
             res.append(col_map[level])
