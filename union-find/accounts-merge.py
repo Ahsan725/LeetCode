6604dca82039b53b4b92ef53d18a2ba7187a5943
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-18 16:10:44 -0400

    LC-accounts-merge [Runtime: 28 ms; Memory: 23.2 MB]

--- union-find/accounts-merge.py
+++ union-find/accounts-merge.py
@@ -1,42 +1,4 @@
-#the right way to solve this is either using a union find or dfs
-class Solution:
-    def accountsMerge(self, accounts):
-        email_to_name = {}
-        merged_accounts = []
-
-        # This list will store sets of emails that have already been grouped
-        groups = []
-
-        for acc in accounts:
-            name = acc[0]
-            emails = set(acc[1:])
-            matched_groups = []
-            
-            # Check existing groups for any email overlap
-            for i, group in enumerate(groups):
-                if emails & group:
-                    matched_groups.append(i)
-
-            # If no group matches, create a new one
-            if not matched_groups:
-                groups.append(emails)
-                for email in emails:
-                    email_to_name[email] = name
-            else:
-                # Merge all matched groups with current emails
-                new_group = emails.copy()
-                for i in reversed(matched_groups):  # reverse to safely remove by index
-                    new_group |= groups[i]
-                    groups.pop(i)
-                groups.append(new_group)
-                for email in new_group:
-                    email_to_name[email] = name  # optional: last name seen wins
-
-        # Build final result
-        for group in groups:
-            sample_email = next(iter(group))
-            name = email_to_name[sample_email]
-            merged_accounts.append([naafrom collections import defaultdict
+from collections import defaultdict
 
 class Solution:
     def accountsMerge(self, accounts):
@@ -70,6 +32,3 @@ class Solution:
                 res.append([email_to_name[email]] + sorted(component))
 
         return res
-me] + sorted(group))
-
-        return merged_accounts
