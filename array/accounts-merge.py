d73694981563df3c34cb0445b0d0dd5d46ac83ca
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-18 16:10:32 -0400

    LC-accounts-merge [Runtime: N/A; Memory: N/A]

--- array/accounts-merge.py
+++ array/accounts-merge.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-
+#the right way to solve this is either using a union find or dfs
 class Solution:
     def accountsMerge(self, accounts):
         email_to_name = {}
@@ -37,6 +36,40 @@ class Solution:
         for group in groups:
             sample_email = next(iter(group))
             name = email_to_name[sample_email]
-            merged_accounts.append([name] + sorted(group))
+            merged_accounts.append([naafrom collections import defaultdict
+
+class Solution:
+    def accountsMerge(self, accounts):
+        email_graph = defaultdict(list)      # email -> list of connected emails
+        email_to_name = {}                   # email -> name
+
+        # Step 1: Build the graph
+        for account in accounts:
+            name = account[0]
+            first_email = account[1]
+            for email in account[1:]:
+                email_graph[first_email].append(email)
+                email_graph[email].append(first_email)
+                email_to_name[email] = name
+
+        visited = set()
+        res = []
+
+        # Step 2: DFS to find connected components (email groups)
+        def dfs(email, component):
+            visited.add(email)
+            component.append(email)
+            for neighbor in email_graph[email]:
+                if neighbor not in visited:
+                    dfs(neighbor, component)
+
+        for email in email_graph:
+            if email not in visited:
+                component = []
+                dfs(email, component)
+                res.append([email_to_name[email]] + sorted(component))
+
+        return res
+me] + sorted(group))
 
         return merged_accounts
