c37e95bf84e6cb50c029ffdb0eaac7efbce3690e
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-18 14:10:23 -0400

    LC-convert-binary-search-tree-to-sorted-doubly-linked-list [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        
+        def inorder_link(self, node):
+            if node:
+                self.inorder_link(node.left)
+                
+                if self.last == None:
+                    self.first = node
+                    self.last = node
+                else:
+                    #set up the links
+                    self.first.left = self.last
+                    self.last.right = self.first
+                    self.last = node
+                    
+                self.inorder_link(node.right)
+        
+        #two main variables first, last 
+        if not root:
+            return None
+        
+        self.first = None
+        self.last = None 
+        
+        self.inorder_link(root)
+        #not sure about which one is the left or right
+        self.first.right = self.last
+        self.last.left = self.first
+        
+        return self.first
