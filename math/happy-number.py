ba7e73d00f9791e0f4f8f9cdbec0f3d556232668
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-22 20:15:09 -0400

    LC-happy-number [Runtime: N/A; Memory: N/A]

---
+++ math/happy-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        #if it is a single number and it is not 1 then we retur false 
+        ans = 0
+        seen = set()
+        temp = n
+        if n < 10 and n != 1:
+            return False 
+        while ans != 1:
+            while temp:
+                last_digit = int(temp %10)
+                ans = int(ans + math.pow(last_digit, 2))
+                temp = int(temp / 10) 
+                if ans in seen:
+                    #do soemthing
+                    return False
+                else:
+                    seen.add(ans)
+            temp = ans
+
+        
+        return True 
