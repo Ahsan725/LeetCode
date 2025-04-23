9e9e52fb05c3b1a4974070f4f5a82406bba9e804
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-22 20:21:20 -0400

    LC-happy-number [Runtime: N/A; Memory: N/A]

--- math/happy-number.py
+++ math/happy-number.py
@@ -7,16 +7,15 @@ class Solution:
         if n < 10 and n != 1:
             return False 
         while ans != 1:
+            ans = 0
             while temp:
-                last_digit = int(temp %10)
+                last_digit = int(temp % 10)
                 ans = int(ans + math.pow(last_digit, 2))
-                temp = int(temp / 10) 
-                if ans in seen:
-                    #do soemthing
-                    return False
-                else:
-                    seen.add(ans)
+                temp = int(temp / 10)
+            if ans in seen:
+                return False
+            else:
+                seen.add(ans)
+            print(ans)
             temp = ans
-
-        
         return True 
