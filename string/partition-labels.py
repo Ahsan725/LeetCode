d21ff9eac000e483d75397fa71f0d9a7719318c5
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-04-28 11:21:45 -0400

    LC-partition-labels [Runtime: 9 ms; Memory: 18 MB]

--- string/partition-labels.py
+++ string/partition-labels.py
@@ -1,17 +1,17 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        lastindex = {}
+        lastindex = {} #this stores the last occurence of each letter
         for i, c in enumerate(s):
             lastindex[c] = i
         
-        res = []
-        size = 0
-        end = 0
+        res = [] 
+        size = 0 #this is what we will append to the res
+        end = 0 #this always mainatins the max of the index of the letter that occured
 
         for i, c in enumerate(s):
-            size += 1
-            end = max(end, lastindex[c])
-            if i >= end:
+            size += 1 #size goes up to every letter to calculate the size of parts
+            end = max(end, lastindex[c]) #maintains the farthest we have to go in 1 part
+            if i == end: #if current index is at 
                 res.append(size)
                 size = 0
         return res 
