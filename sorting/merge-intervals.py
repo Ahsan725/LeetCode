04c82a63ab89d221fdbe641ff2f9bc78df837ce3
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-05-08 15:25:05 -0400

    LC-merge-intervals [Runtime: 7 ms; Memory: 21.8 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -1,18 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
-        intervals.sort()
+        #first thing about interval based problems is seeing if sorting will help us figure our the solution 
+        res = []
+        intervals.sort() #no need to do the extra lambda because it does the first val by default 
         res = [intervals[0]]
-
-        #intervals = [[1,4],[4,5]]
-
-        for start, end in intervals:
-            last_end_value = res[-1][1]
-
-            if start <= last_end_value:
-                res[-1][1] = max(res[-1][1], end)
+        
+        for start, end in intervals[1:]:
+            prevend = res[-1][1] #get the last entry and its end value 
+            
+            if start <= prevend:
+                res[-1][1] = max(prevend, end)
             else:
                 res.append([start, end])
-        return res
-
-        
+        return res 
