ce6cf9d54d2984c39e5e0466a957a94d7e080045
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-05-08 16:32:05 -0400

    LC-word-search [Runtime: 4327 ms; Memory: 18.1 MB]

--- depth-first-search/word-search.py
+++ depth-first-search/word-search.py
@@ -1,26 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        #very similar to number of islands 
-        path = set()
+        #mostly the same structure as a regaulr dfs problem 
         rows = len(board)
         cols = len(board[0])
+        path = set()
 
-        def dfs(r,c,i):
-            if i == len(word):
-                return True
-            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in path or board[r][c] != word[i]:
-                return False
+        #dfs
+        def dfs(r, c, i):
+            if r < 0 or c < 0 or r >= rows or c >= cols or i >= len(word) or board[r][c] != word[i] or (r,c) in path:
+                return False 
+            if i == len(word) - 1:
+                return True 
             
-
             path.add((r,c))
-
-            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1 , i + 1)
+            res = dfs(r + 1,c, i + 1) or dfs(r - 1,c, i + 1) or dfs(r,c + 1, i + 1) or dfs(r,c - 1, i + 1)
             path.remove((r,c))
-
             return res 
-        
+
+
+
+        #iterative 
         for r in range(rows):
             for c in range(cols):
-                if dfs(r,c, 0):
-                    return True
-        return False
+                if dfs(r, c, 0):
+                    return True 
+        return False 
