9afae39df1601eb1dfc693995b46a63b1ededfeb
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-05-09 12:21:03 -0400

    LC-valid-parentheses [Runtime: 0 ms; Memory: 18 MB]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -3,16 +3,11 @@ class Solution:
         pairs = {")": "(", "}": "{", "]": "["}
         stack = []
 
-        #edge cases
-        #input too small
-        # if len(s) < 2:
-        #     return False
-
         for c in s:
             if c in ")}]":
                 #we found a closing parenthesis 
                 if not stack or pairs[c] != stack[-1]:
-                    #we did not find correct matching
+                    #we did not find correct matching or found a closing paren before an opening 
                     return False
                 else:
                     if stack:
