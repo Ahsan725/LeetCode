d48170792ccf598bd204e924b2975b91b274e29e
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-05-09 11:51:24 -0400

    LC-word-search [Runtime: 4177 ms; Memory: 18.1 MB]

--- backtracking/word-search.py
+++ backtracking/word-search.py
@@ -1,27 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        #mostly the same structure as a regaulr dfs problem 
+        #variables
         rows = len(board)
         cols = len(board[0])
         path = set()
-
+        
         #dfs
-        def dfs(r, c, i):
-            if r < 0 or c < 0 or r >= rows or c >= cols or i >= len(word) or board[r][c] != word[i] or (r,c) in path:
-                return False 
+        def dfs(r,c, i):
+            if r < 0 or c < 0 or r >= rows or c >= cols or word[i] != board[r][c] or (r,c) in path:
+                return False
             if i == len(word) - 1:
                 return True 
-            
+
+            #some step you have to do before calling the dfs more times
             path.add((r,c))
-            res = dfs(r + 1,c, i + 1) or dfs(r - 1,c, i + 1) or dfs(r,c + 1, i + 1) or dfs(r,c - 1, i + 1)
+            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
             path.remove((r,c))
             return res 
-
-
 
         #iterative 
         for r in range(rows):
             for c in range(cols):
-                if dfs(r, c, 0):
+                if dfs(r,c,0):
                     return True 
         return False 
+                
