77a83cb2cdffeb240fb963050db07cd3abe38697
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-05-24 22:50:42 -0400

    LC-count-largest-group [Runtime: 74 ms; Memory: 17.9 MB]

--- math/count-largest-group.py
+++ math/count-largest-group.py
@@ -1,34 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        #use a map key:number -> [numbers that add up to that number]
-        num_to_list = defaultdict(list)
+        digit_sum_count = defaultdict(int)
 
         for i in range(1, n + 1):
-            if i <=9:
-                summ = i
-            else:
-                string_val = str(i)
-                summ = 0
-                for c in string_val:
-                    summ += int(c)
-                # print(summ)
+            digit_sum = sum(int(c) for c in str(i))
+            digit_sum_count[digit_sum] += 1
 
-            num_to_list[summ].append(i)
-        
-        #find the largest number of elemnts in a group and then how many groups hve that 
-        max_count = float('-inf')
+        max_count = max(digit_sum_count.values())
+
         res = 0
-        for lists in num_to_list.values():
-            count = 0 
-            for num in lists:
-                count += 1
-            max_count = max(max_count, count)
-        
-        for lists in num_to_list.values():
-            count = 0
-            for num in lists:
-                count += 1
+        for count in digit_sum_count.values():
             if count == max_count:
                 res += 1
+                
         return res
-            
