2f5607b0f62553c15c61a97ba5f0986e076812a2
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-06-13 13:09:34 -0400

    LC-binary-tree-vertical-order-traversal [Runtime: N/A; Memory: N/A]

--- sorting/binary-tree-vertical-order-traversal.py
+++ sorting/binary-tree-vertical-order-traversal.py
@@ -1,36 +1,29 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        #idea of the algo is that root we consider (0,0) cordinate if we go left a col thats -1 if we go right + 1
-
-        """
-        Approach:
-        - Use BFS to traverse the tree while tracking horizontal distances (x-coordinates).
-        - Use a hashmap (col_map) to group node values by their column index.
-        - Keep track of min and max x to build the result in left-to-right column order.
-        """
-        if not root:
-            return []
-
+        #will use bfs 
+        #x cordinate method 
         res = []
         col_map = defaultdict(list)
-        
-        minx = float('inf')
-        maxx = float('-inf')
-        q = deque([(0, root)])
+        q = deque((0, root))
         
         while q:
-            col, node = q.popleft()
+            node, x = q.popleft()
+            col_map[x].append(node.val)
             
-            col_map[col].append(node.val)
-            minx = min(minx, col)
-            maxx = max(maxx, col)
             
+            #add the children
             if node.left:
-                q.append((col - 1, node.left))
+                q.append((x - 1, node))
             if node.right:
-                q.append((col + 1, node.right))
+                q.append((x + 1, node))
+                
+        #popping elemnts in the right roder in the result 
+        sorted_items = sorted(col_map.items())
+        for col, val in sorted_items:
+            res.append(col_map[col])
+        return res
             
-        for level in range(minx, maxx + 1):
-            res.append(col_map[level])
-        return res 
             
+            
+            
+        
