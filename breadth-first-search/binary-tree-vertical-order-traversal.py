0364efd012af3ec87904fcd85bf69382f8504760
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-06-13 13:14:15 -0400

    LC-binary-tree-vertical-order-traversal [Runtime: 0 ms; Memory: 17.8 MB]

--- breadth-first-search/binary-tree-vertical-order-traversal.py
+++ breadth-first-search/binary-tree-vertical-order-traversal.py
@@ -5,13 +5,16 @@ class Solution:
         if not root:
             return []
         res = []
+        minx = float('inf')
+        maxx = float('-inf')
         col_map = defaultdict(list)
         q = deque([(0, root)])
         
         while q:
             x, node = q.popleft()
             col_map[x].append(node.val)
-            
+            minx = min(minx, x)
+            maxx = max(maxx, x)
             
             #add the children
             if node.left:
@@ -20,11 +23,17 @@ class Solution:
                 q.append((x + 1, node.right))
                 
         #popping elemnts in the right roder in the result 
-        sorted_items = sorted(col_map.items())
-        for col, val in sorted_items:
-            res.append(col_map[col])
+        # sorted_items = sorted(col_map.items())
+        # for col, val in sorted_items:
+        #     res.append(col_map[col])
+        # return res
+
+        for i in range(minx, maxx + 1):
+            res.append(col_map[i])
         return res
             
+        #Time: O(nlogn) -> this from sorting
+        #Space: O(n)
             
             
             
