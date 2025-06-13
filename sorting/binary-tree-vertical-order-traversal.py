a3bfd91e14e176bf5d11c144f6e0b4564d42d152
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-06-13 13:11:29 -0400

    LC-binary-tree-vertical-order-traversal [Runtime: 0 ms; Memory: 17.9 MB]

--- sorting/binary-tree-vertical-order-traversal.py
+++ sorting/binary-tree-vertical-order-traversal.py
@@ -2,20 +2,22 @@ class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         #will use bfs 
         #x cordinate method 
+        if not root:
+            return []
         res = []
         col_map = defaultdict(list)
-        q = deque((0, root))
+        q = deque([(0, root)])
         
         while q:
-            node, x = q.popleft()
+            x, node = q.popleft()
             col_map[x].append(node.val)
             
             
             #add the children
             if node.left:
-                q.append((x - 1, node))
+                q.append((x - 1, node.left))
             if node.right:
-                q.append((x + 1, node))
+                q.append((x + 1, node.right))
                 
         #popping elemnts in the right roder in the result 
         sorted_items = sorted(col_map.items())
