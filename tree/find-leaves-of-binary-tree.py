4f35e5a470069d6dc12e996dc7460a1b5f731334
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-06-20 10:51:15 -0400

    LC-find-leaves-of-binary-tree [Runtime: 0 ms; Memory: 17.7 MB]

--- tree/find-leaves-of-binary-tree.py
+++ tree/find-leaves-of-binary-tree.py
@@ -8,13 +8,14 @@ from collections import defaultdict
 
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        level_map = defaultdict(list)
+        level_map = defaultdict(list) #to store leaves based on removals 
 
         def dfs(node):
             if not node:
-                return -1
+                return -1 #this should not contribute anything to the final rsult since the first lvl will be 0
             left = dfs(node.left)
             right = dfs(node.right)
+            
             height = max(left, right) + 1
             level_map[height].append(node.val)
             return height
