b4f14d461c25b9da2541d5b209f185e9d8e5a08e
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-06-24 16:04:44 -0400

    LC-find-all-k-distant-indices-in-an-array [Runtime: 809 ms; Memory: 17.9 MB]

--- array/find-all-k-distant-indices-in-an-array.py
+++ array/find-all-k-distant-indices-in-an-array.py
@@ -1,8 +1,6 @@
 class Solution:
-    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
-        
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]: 
         res = set()
-
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if abs(i - j) <= k and nums[j] == key:
