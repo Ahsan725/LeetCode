cc6fd25da564f69113c96f383d3e204643253a76
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-08-08 09:26:35 -0400

    LC-lru-cache [Runtime: 101 ms; Memory: 78.1 MB]

--- linked-list/lru-cache.py
+++ linked-list/lru-cache.py
@@ -10,15 +10,12 @@ class LRUCache:
         else:
             self.cache.move_to_end(key)
             return self.cache[key]
-        
 
     def put(self, key: int, value: int) -> None:
         self.cache[key] = value
         self.cache.move_to_end(key)
         if len(self.cache) > self.capacity:
             self.cache.popitem(last = False)
-
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
