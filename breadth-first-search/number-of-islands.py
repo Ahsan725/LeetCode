56dfda1c5db2e0719937fc2b04494494002d2f58
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-08-08 08:48:57 -0400

    LC-number-of-islands [Runtime: 222 ms; Memory: 20.1 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -1,23 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #two parts: dfs, itertaive part 
+        #Understanding: we are counting islands and everytime we see a connected island that is counted as 1 
+        #Approch: iterate over matrix and anytime we see a '1' we want to call a dfs function to mark all connected islands as just one island. so we don;t over count
+        
         rows = len(grid)
         cols = len(grid[0])
         res = 0
-
-
-        #dfs 
-        def dfs(r, c):
-            if r >= rows or c >= cols or r < 0 or c < 0 or grid[r][c] == '0':
+        
+        def dfs(r,c):
+            #check boundaries and base condition
+            if r >= rows or c >= cols or r<0 or c<0 or grid[r][c] == '0':
                 return 
+            #marking 
             grid[r][c] = '0'
-
-            dirs = [(1,0), (0, 1), (-1, 0), (0, -1)]
-            for dr, dc in dirs:
-                dfs(r + dr, c + dc)
-
-
-        #iterative 
+            
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+        
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
