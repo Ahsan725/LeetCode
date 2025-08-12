8697fc035be29ff637b9ff25ff26f63bae2ad296
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-08-12 12:00:59 -0400

    LC-surrounded-regions [Runtime: 8 ms; Memory: 21.6 MB]

--- breadth-first-search/surrounded-regions.py
+++ breadth-first-search/surrounded-regions.py
@@ -1,24 +1,5 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        rows = len(board)
-        cols = len(board[0])
-
-        def dfs(r,c):
-            #boundary condtion
-            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] == "X" or r == rows - 1 or c == cols -1:
-                return
-
-            board[r][c] = 'T'
-
-            #dfs call
-            dfs(r + 1, c)
-            dfs(r - 1,c)
-            dfs(r,c + 1)
-            dfs(r,c - 1)aclass Solution:
-    def solve(self, board: List[List[str]]) -> None:
         rows, cols = len(board), len(board[0])
 
         def dfs(r, c):
@@ -47,17 +28,3 @@ class Solution:
             for c in range(cols):
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
-
-
-
-
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "O":
-                    dfs(r,c)
-        
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "T":
-                    board[r][c] = 'X'
-        
