e77571ed2e800b8016438cd608a96de8430595a5
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-08-12 11:55:41 -0400

    LC-surrounded-regions [Runtime: N/A; Memory: N/A]

--- matrix/surrounded-regions.py
+++ matrix/surrounded-regions.py
@@ -5,7 +5,7 @@ class Solution:
         cols = len(board[0])
         
         def dfs(r,c):
-            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] == 'O' or r == rows -1 or c == cols -1:
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] == 'X' or r == rows -1 or c == cols -1:
                 return 
             # board[r][c] = 'X'
             
@@ -16,7 +16,7 @@ class Solution:
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'X':
+                if board[r][c] == 'O':
                     dfs(r,c) 
                     
         
