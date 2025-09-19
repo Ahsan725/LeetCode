de61b3b7d328fa01503534c5f57bb0c46dc42c69
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-09-19 08:19:50 -0400

    LC-add-two-numbers [Runtime: 4 ms; Memory: 18 MB]

--- linked-list/add-two-numbers.py
+++ linked-list/add-two-numbers.py
@@ -5,23 +5,31 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #we are returning a linked list
+        #do I need a dummy head?
+        #do I need a cur variable?
+        #am I returning dummy or head or something else?
         
-        carry = 0 
-        dummy = ListNode()
-        cur = dummy
+        carry = 0
+        dummy = ListNode() # added a new node before
+        cur = dummy #this is like saying cur is AT dummy
         
         while l1 or l2 or carry:
+            #is it correct to use or here?
             num1 = l1.val if l1 else 0
             num2 = l2.val if l2 else 0
-            
             total = num1 + num2 + carry
-            carry = total // 10
-            val = total % 10
-            cur.next = ListNode(val)
+            #is there a carry?
+            carry = total // 10 
+            value = total % 10
             
+            #attaching
+            cur.next = ListNode(value)
+            cur = cur.next
+            
+            #increment pointers
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            cur = cur.next
-        
+            
         return dummy.next
         
