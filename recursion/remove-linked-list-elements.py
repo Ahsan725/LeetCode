fbdc6ad9c4bc16c5a10f9ee7a636bd9d3c6e8cd3
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-10-16 17:51:34 -0400

    LC-remove-linked-list-elements [Runtime: 0 ms; Memory: 19.6 MB]

--- recursion/remove-linked-list-elements.py
+++ recursion/remove-linked-list-elements.py
@@ -5,17 +5,16 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        cur = ListNode()
+        
         dummy = ListNode()
         dummy.next = head #in case we have to delete the head
         cur = head
         prev = dummy
         while cur:
-            if cur and cur.val == val:
+            if cur.val == val:
                 prev.next = cur.next
             else:
                 prev = cur
             cur = cur.next
-            
 
         return dummy.next
