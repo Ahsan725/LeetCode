da3e4c5fe04a523f5955633218db60ee1cbfe7c4
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2025-10-22 20:47:44 -0400

    LC-implement-magic-dictionary [Runtime: N/A; Memory: N/A]

---
+++ design/implement-magic-dictionary.py
@@ -0,0 +1,28 @@
+class MagicDictionary:
+
+    def __init__(self):
+        self.magic = []
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        self.magic.extend(dictionary)
+
+    def search(self, searchWord: str) -> bool:
+        for word in self.magic:
+            if len(word) == len(searchWord) and word != searchWord:
+                i = count = 0
+                while i < len(word):
+                    if word[i] != searchWord[i]:
+                        count += 1
+                    if count > 1:
+                        break
+                    i += 1
+                if count == 1:
+                    return True
+                else:
+                    return False
+        return False
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
